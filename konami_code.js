15e58ba05465daba38012def6a3b3b8d68593f57
Travis Martin <travisemerymartin@gmail.com>
2018-12-24 06:41:10 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -19,12 +19,12 @@ function init() {
       console.log(`input ${input}`)
       console.log(`correctInput ${correctInput}`)
       console.log(`index ${index}`)
-    if (index===codes.length) {
+    if (input.toString()===correctInput.toString()) {
+      index++;
+    }
+    else if (index===codes.length {
       alert("Code Successfully Entered");
       return index=0;
-    }
-    else if (input.toString()===correctInput.toString()) {
-      index++;
     }
     else {
       index=0
